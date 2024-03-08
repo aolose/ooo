700f024f02c1f334edf766d2d8d2d54a5fd5748c
tom <i@aolose.cc>
2024-03-09 02:58:06 +0800

    check R2

--- src/lib/server/apis.ts
+++ src/lib/server/apis.ts
@@ -1,8 +1,13 @@
 import type {APIRoute} from '../../ambient';
 import type {RequestEvent} from '@sveltejs/kit';
-import {putObj} from "$lib/server/r2";
+import {putObj, R2} from "$lib/server/r2";
 
 export const Apis: APIRoute = {
+    ok:{
+      GET(){
+          return R2.bucket?1:0
+      }
+    },
     test: {
         GET() {
             return 'hello world';
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import {submit} from "$lib/form";
+    import {onMount} from "svelte";
 
     let link = ''
     let name = ''
+    let bucketOk = 0
     const done = async (r: Response) => {
         link = await r.text()
     }
@@ -10,8 +12,13 @@
         const fi = f.get('file') as File
         name = fi.name
     }
+
+    onMount(() => {
+        fetch('/api/ok').then(async a => bucketOk = +await a.text())
+    })
 </script>
 <div>
+    <p>bucketOk:{bucketOk}</p>
     <form on:submit={submit(done,before)} method="post" action="/api/upload">
         <input name="file" type="file">
         <input type="submit" value="submit">
