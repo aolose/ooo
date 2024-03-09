0eed92ecfd9e1acf3b10d212652a623fcd5654a9
tom <anytable@gmail.com>
2024-03-09 23:05:42 +0800

    abstract storage interface

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,37 +1,11 @@
 import type {Handle} from "@sveltejs/kit";
 import {apiHandler} from "$lib/server/apiRouter";
 import {filter} from "$lib/server/firewall";
-import {dev} from "$app/environment";
-import type {Miniflare} from "miniflare";
-import {initR2} from "$lib/server/r2";
 
-let mf: Miniflare;
+import {storage} from "$lib/server/storage";
+
 export const handle: Handle = async ({event, resolve}) => {
-    if (dev) {
-        if (!mf) {
-            const {Miniflare, Log, LogLevel} = await import("miniflare");
-            mf = new Miniflare({
-                log: new Log(LogLevel.INFO),
-                // kvPersist: "./kv-data",
-                // kvNamespaces: ["KV"],
-                // d1Persist: "./d1-data",
-                // d1Databases: ["D1"],
-                r2Buckets: ["ooo"],
-                // you should also be able to add durable objects & r2
-                script: "",
-                modules: true,
-            });
-        }
-      
-        event.platform = {
-            env: {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                MY_BUCKET: await mf.getR2Bucket('ooo')
-            }
-        };
-    }
-    await initR2(event.platform?.env.MY_BUCKET)
+    storage.init(event.platform?.env)
     if (event.url.pathname.startsWith('/api/')) {
         return apiHandler(event)
     }
