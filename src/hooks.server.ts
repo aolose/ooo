4ad408ae24991ae05689195738b645dcbabcfd73
tom <anytable@gmail.com>
2024-04-02 21:59:24 +0800

    update ws

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,20 +6,15 @@ import { execSchema } from '$lib/server/schema';
 import { Apis } from '$lib/server/apis';
 import { handleUpgrade } from 'vite-sveltekit-cf-ws';
 
-let once = 0;
-const initSockets = () => {
-	once++
-	handleUpgrade((req, createWs) => {
-		const pathname = new URL(req.url || '', 'ws://base.url').pathname;
-		const fn = Apis[pathname.slice(5)]?.WS;
-		if (fn) {
-			fn(createWs());
-		}
-	});
-};
+handleUpgrade((req, createWs) => {
+	const pathname = new URL(req.url || '', 'ws://base.url').pathname;
+	const fn = Apis[pathname.slice(5)]?.WS;
+	if (fn) {
+		fn(createWs());
+	}
+});
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (!once) initSockets();
 	await connect(event.platform?.env, execSchema);
 	if (event.url.pathname.startsWith('/api/')) {
 		return apiHandler(event);
@@ -27,4 +22,4 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const res = filter(event);
 	if (res) return res;
 	return resolve(event);
-};
+}
